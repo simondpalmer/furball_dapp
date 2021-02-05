17f5893fd484d2d723bbcd4f8b6fa617c0cf6735
Lev Stambler <levstamb@gmail.com>
2021-02-05 17:22:46 -0500

    integrate NEP21

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -3,11 +3,14 @@ use near_sdk::{
     collections::{UnorderedMap, UnorderedSet},
     env,
     json_types::U128,
-    near_bindgen, AccountId,
+    near_bindgen, wee_alloc, AccountId,
 };
 
 mod account;
+mod error;
 mod nep21;
+
+use error::Error;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -25,8 +28,8 @@ pub trait FungibleTokenTrait {
         amount: U128,
     );
     fn transfer(&mut self, art: CID, new_owner_id: AccountId, amount: U128);
-    fn get_total_supply(&self, art: CID);
-    fn get_balance(&self, art: CID, owner_id: AccountId);
+    fn get_total_supply(&self, art: CID) -> U128;
+    fn get_balance(&self, art: CID, owner_id: AccountId) -> U128;
     fn get_allowance(&self, art: CID, owner_id: AccountId, escrow_account_id: AccountId) -> U128;
 }
 
@@ -34,7 +37,7 @@ pub trait FungibleTokenTrait {
 pub struct Token {
     artist: account::Account,
     artwork: CID,
-    token_info: nep21::FungibleToken,
+    token: nep21::FungibleToken,
 }
 
 #[near_bindgen]
@@ -62,12 +65,59 @@ impl FurBall {
     }
 }
 
+impl FurBall {
+    fn get_art(&self, art: &CID) -> Result<Token, Error> {
+        // TODO: err msg
+        self.artCIDToToken
+            .get(&art)
+            .ok_or(Error::ArtCIDNotFound(*art))
+    }
+}
+
 /// Add fungible token functionality for individual tokens
 #[near_bindgen]
 impl FungibleTokenTrait for FurBall {
+    #[payable]
     fn inc_allowance(&mut self, art: CID, escrow_account_id: AccountId, amount: U128) {
-        // TODO: err msg
-        let artToken = self.artCIDToToken.get(&art).unwrap();
-        artToken.token_info.inc_allowance(escrow_account_id, amount);
+        let mut art_token = self.get_art(&art).unwrap();
+        art_token.token.inc_allowance(escrow_account_id, amount);
+    }
+    #[payable]
+    fn dec_allowance(&mut self, art: CID, escrow_account_id: AccountId, amount: U128) {
+        let mut art_token = self.get_art(&art).unwrap();
+        art_token.token.dec_allowance(escrow_account_id, amount);
+    }
+    #[payable]
+    fn transfer_from(
+        &mut self,
+        art: CID,
+        owner_id: AccountId,
+        new_owner_id: AccountId,
+        amount: U128,
+    ) {
+        let mut art_token = self.get_art(&art).unwrap();
+        art_token
+            .token
+            .transfer_from(owner_id, new_owner_id, amount);
+    }
+    #[payable]
+    fn transfer(&mut self, art: CID, new_owner_id: AccountId, amount: U128) {
+        let mut art_token = self.get_art(&art).unwrap();
+        art_token.token.transfer(new_owner_id, amount);
+    }
+    fn get_total_supply(&self, art: CID) -> U128 {
+        let art_token = self.get_art(&art).unwrap();
+        art_token.token.get_total_supply()
+    }
+
+    fn get_balance(&self, art: CID, owner_id: AccountId) -> U128 {
+        let art_token = self.get_art(&art).unwrap();
+        art_token.token.get_balance(owner_id)
+    }
+    fn get_allowance(&self, art: CID, owner_id: AccountId, escrow_account_id: AccountId) -> U128 {
+        let art_token = self.get_art(&art).unwrap();
+        art_token
+            .token
+            .get_allowance(owner_id, escrow_account_id)
     }
 }
