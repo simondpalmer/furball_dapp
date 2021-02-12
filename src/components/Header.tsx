6269757e56d25b6453974314c43a31bad4f83c46
Sebastien La Duca <sladuca777@gmail.com>
2021-02-11 23:56:26 -0500

    add drawer and upload lookup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
+import { List, ListItem } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AddBox from '@material-ui/icons/AddBox';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import ArtistForm from '../form/ArtistForm';
 import { login, logout } from "../utils";
 import BoxButton from './controls/Button';
@@ -65,6 +67,12 @@ const useStyles = makeStyles((theme) => ({
       display: 'block',
     },
   },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
 }));
 
 export interface HeaderProps {
@@ -77,15 +85,58 @@ export function Header(auth: HeaderProps) {
   const [openPopup, setOpenPopup] = useState(false);
   const [redirect, setRedirect] = useState(false);
   const [searchCid, setSearchCid] = useState('');
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
 
   const handleSubmit = () => {
     setRedirect(true)
   }
 
   if (redirect) {
-    // setRedirect(false)
     return <Redirect push to={`/artwork/:${searchCid}`} />
   }
+
+  const setDrawerState = (newDrawerState: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setDrawerOpen(newDrawerState);
+  };
+
+  const drawerList = (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={setDrawerState(true)}
+      onKeyDown={setDrawerState(false)}
+    >
+      <List>
+        <ListItem button key="Home">
+          <Link to="/ ">
+            Home
+          </Link>
+        </ListItem>
+        <ListItem button key="Lookup">
+          <Link to="/lookup">
+            Lookup
+          </Link>
+        </ListItem>
+        <ListItem button key="Gallery">
+          <Link to="/gallery">
+            Gallery
+          </Link>
+        </ListItem>
+      </List>
+    </div>
+  )
 
   const rhs = auth ? (
     <>
@@ -106,9 +157,18 @@ export function Header(auth: HeaderProps) {
     <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={setDrawerState(true)}>
             <MenuIcon />
           </IconButton>
+
+          <SwipeableDrawer
+            anchor="left"
+            open={drawerOpen}
+            onClose={setDrawerState(false)}
+            onOpen={setDrawerState(true)}
+          >
+            {drawerList}
+          </SwipeableDrawer>
           <Typography variant="h6" className={classes.title}>
             Furball
           </Typography>
--- src/pages/Lookup.tsx
+++ src/pages/Lookup.tsx
@@ -1,6 +1,7 @@
 import Paper from '@material-ui/core/Paper';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { Redirect } from 'react-router-dom';
 //@ts-ignore
 import stegasus from "../../../stegasus/Cargo.toml";
 import { getArtworkCidFromOriginalCid } from '../api/token';
@@ -10,6 +11,8 @@ export interface LookupProps { }
 export function Lookup(props: LookupProps) {
 
   const [cid, setCID] = useState<string | null>(null);
+  const [redirect, setRedirect] = useState(false);
+
 
   const handleLookup = async (buf: Buffer) => {
     const imgBuf = new Uint8Array(buf);
@@ -17,9 +20,14 @@ export function Lookup(props: LookupProps) {
     const originalCID = Buffer.from(stegoData).toString();
     console.log(originalCID);
     const artCid = await getArtworkCidFromOriginalCid(originalCID)
-
-    // TODO redirect to artCID
+    setCID(artCid)
   }
+
+  useEffect(() => {
+    if (cid) {
+      setRedirect(true)
+    }
+  }, [cid])
 
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     accept: "image/png",
@@ -41,24 +49,18 @@ export function Lookup(props: LookupProps) {
     }
   })
 
-  const file = acceptedFiles[0] as any
-  const fileItem = (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  )
+  if (redirect) {
+    return <Redirect push to={`/artwork/:${cid}`} />
+  }
+
 
   return (
     <Paper>
       <section className="container">
         <div {...getRootProps({ className: 'dropzone' })}>
           <input {...getInputProps()} />
-          <p>Upload an image to search the Furball registry for</p>
+          <p>Upload an image to search in the Furball registry</p>
         </div>
-        <aside>
-          <h4>Files</h4>
-          <ul>{[fileItem]}</ul>
-        </aside>
       </section>
     </Paper>
   )
